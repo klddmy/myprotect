38dafb69fe730e00903c8e5de510324d220f46cb
klddmy <1559621968@qq.com>
2020-03-31 10:11:38 +0800

    作业3 35-王振权 finish time stamp

--- post_youdao.py
+++ post_youdao.py
@@ -2,15 +2,33 @@ import requests
 
 url = "http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
 
+
+def get_salt():
+    return '15846849464028'
+
+
+def get_sign():
+    return 'c0602e8a7ec7eface095889cad4926f0'
+
+
+def get_ts():
+    import time
+    t = time.time()
+    ts = str(int(round(t * 1000)))
+    print(ts)
+    return ts
+         # "1585618581774"
+
+
 form_data={
     'i': '我和你都是',
     'from': 'AUTO',
     'to': 'AUTO',
     'smartresult': 'dict',
     'client': 'fanyideskweb',
-    'salt': '15846849464028',
-    'sign': 'c0602e8a7ec7eface095889cad4926f0',
-    'ts': '1584684946402',
+    'salt': get_salt(),
+    'sign': get_sign(),
+    'ts': get_ts(),
     'bv': '06aea2cce1c727ac4ac3cd80067ea894',
     'doctype': 'json',
     'version': '2.1',
